3fa3a6d140f6562ae2cc370ce4562bfa7fe3b008
Pratik Kshirsagar <meetpratiik1995@gmail.com>
2022-07-24 20:01:36 +0530

    vue router added

--- tests/unit/components/Naviagtion/MainNav.spec.js
+++ tests/unit/components/Naviagtion/MainNav.spec.js
@@ -1,15 +1,23 @@
-import { shallowMount } from '@vue/test-utils';
+import { RouterLinkStub, shallowMount } from '@vue/test-utils';
 
 import MainNav from '@/components/Navigation/MainNav';
 
 describe('mainNav', () => {
+  const createConfig = () => ({
+    global: {
+      subus: {
+        'router-link': RouterLinkStub,
+      },
+    },
+  });
+
   it('displays company name', () => {
-    const wrapper = shallowMount(MainNav);
+    const wrapper = shallowMount(MainNav, createConfig());
     expect(wrapper.text()).toMatch('Bobo Careers');
   });
 
   it('display menu items for navigation', () => {
-    const wrapper = shallowMount(MainNav);
+    const wrapper = shallowMount(MainNav, createConfig());
     const navigationMenuItems = wrapper.findAll(
       `[data-test='main-nav-list-item']`
     );
@@ -26,7 +34,7 @@ describe('mainNav', () => {
 
   describe('when user is logged out', () => {
     it('prompts user to sign in', () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig());
 
       const loginButton = wrapper.find(`[data-test='login-button']`);
       expect(loginButton.exists()).toBe(true);
@@ -35,7 +43,7 @@ describe('mainNav', () => {
 
   describe('when user is logged in', () => {
     it('display user profile picture', async () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig());
       let profileImage = wrapper.find(`[data-test='profile-image']`);
       expect(profileImage.exists()).toBe(false);
 
@@ -47,7 +55,7 @@ describe('mainNav', () => {
     });
 
     it('diaplays subnavigation with additional informations', async () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, createConfig());
 
       let subnav = wrapper.find(`[data-test='subnav']`);
       expect(subnav.exists()).toBe(false);
